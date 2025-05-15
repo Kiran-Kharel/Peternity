215abca8fe6cea367944d6c5898df4587f7716cc
LasataBjr <lasatabjrktm@gmail.com>
2025-05-15 19:39:49 +0545

    update 1

--- signup.php
+++ signup.php
@@ -3,21 +3,78 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="Assets/CSS/sigup.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="Assets/CSS/signup.css">
+    <link rel="stylesheet" href="style.css">
     <title>Sign Up</title>
 </head>
 <body>
-    <form action="" method = "POST" class="signin">
-        <h1>Sign Up</h1>
-        <div class="signin_form">
-            <input type="text" name="username"/>
-            <input type="password" name="password"/>
-            <input type="password" name="confirmpassword"/>
-            <input type="file" name="userProfile"/>
-            <input type="text" name="address"/>
-            <input type="email" name="email"/>
+    <div class="signin_div">
+        <form action="" method = "POST" class="signup">
+            <h1>Sign Up</h1>
+            
+                <input type="text" name="username" placeholder= "Name"/><br>
+                <input type="password" name="password" placeholder= "Password"/><br>
+                <input type="password" name="confirmpassword" placeholder= "RePassword"/><br>
+                <input type="file" name="userProfile" placeholder= "Profile Picture"/><br>
+                <input type="text" name="address" placeholder= "Address"/><br>
+                <input type="email" name="email" placeholder= "Email"/><br>
+                <input type="submit" value="Submit">
+        </form>
+    </div>
+    <!-- <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form>
+                <h1>Create Account</h1>
+                <div class="social-icons">
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your email for registeration</span>
+                <input type="text" placeholder="Name">
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Password">
+                <button>Sign Up</button>
+            </form>
         </div>
-    </form>
+        <div class="form-container sign-in">
+            <form>
+                <h1>Sign In</h1>
+                <div class="social-icons">
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your email password</span>
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Password">
+                <a href="#">Forget Your Password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Welcome Back!</h1>
+                    <p>Enter your personal details to use all of site features</p>
+                    <button class="hidden" id="login">Sign In</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Register with your personal details to use all of site features</p>
+                    <button class="hidden" id="register">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="script.js"></script> -->
     
 </body>
 </html>
+
+
+    
