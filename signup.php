13d5131a90dded27a6db9e71493822e61e86dfd1
LasataBjr <lasatabjrktm@gmail.com>
2025-05-15 16:50:51 +0545

    added signup page

---
+++ signup.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Assets/CSS/sigup.css">
+    <title>Sign Up</title>
+</head>
+<body>
+    <form action="" method = "POST" class="signin">
+        <h1>Sign Up</h1>
+        <div class="signin_form">
+            <input type="text" name="username"/>
+            <input type="password" name="password"/>
+            <input type="password" name="confirmpassword"/>
+            <input type="file" name="userProfile"/>
+            <input type="text" name="address"/>
+            <input type="email" name="email"/>
+        </div>
+    </form>
+    
+</body>
+</html>
