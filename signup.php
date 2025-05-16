9f8a6784a42d0cb7670282ac6f7e3fd59685d02a
LasataBjr <lasatabjrktm@gmail.com>
2025-05-16 19:21:57 +0545

    updated forms

--- signup.php
+++ signup.php
@@ -5,73 +5,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="Assets/CSS/signup.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Assets/css/signup.css">
     <title>Sign Up</title>
 </head>
 <body>
-    <div class="signin_div">
-        <form action="" method = "POST" class="signup">
-            <h1>Sign Up</h1>
-            
-                <input type="text" name="username" placeholder= "Name"/><br>
-                <input type="password" name="password" placeholder= "Password"/><br>
-                <input type="password" name="confirmpassword" placeholder= "RePassword"/><br>
-                <input type="file" name="userProfile" placeholder= "Profile Picture"/><br>
-                <input type="text" name="address" placeholder= "Address"/><br>
-                <input type="email" name="email" placeholder= "Email"/><br>
-                <input type="submit" value="Submit">
-        </form>
-    </div>
-    <!-- <div class="container" id="container">
-        <div class="form-container sign-up">
-            <form>
-                <h1>Create Account</h1>
-                <div class="social-icons">
-                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
-                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
-                    <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
-                    <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+    <div class="form-container" id="form-container">
+        <div class="sign-up">
+            <form action="" >
+                <h1>Sign Up</h1>                
+                    <input type="text" name="username" placeholder= "Name"/><br>
+                    <input type="password" name="password" placeholder= "Password"/><br>
+                    <input type="password" name="confirmpassword" placeholder= "RePassword"/><br>
+                    <label for="userProfile">User Profile:</label><br>
+                    <input type="file" name="userProfile" placeholder= "Profile Picture"/><br>
+                    <input type="text" name="address" placeholder= "Address"/><br>
+                    <input type="email" name="email" placeholder= "Email"/><br>
+                    <button class="form-button">Sign Up</button><br>
+                    <span>---</span> <span>or</span> <span>---</span>
+                <div class="social-icons"> 
+                    <a href="#" class="icon"><i class="fab fa-google"></i></a>
+                    <a href="#" class="icon"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
                 </div>
-                <span>or use your email for registeration</span>
-                <input type="text" placeholder="Name">
-                <input type="email" placeholder="Email">
-                <input type="password" placeholder="Password">
-                <button>Sign Up</button>
             </form>
         </div>
-        <div class="form-container sign-in">
-            <form>
-                <h1>Sign In</h1>
-                <div class="social-icons">
-                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
-                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
-                    <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
-                    <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+        <div class="sign-in">
+            <form action="">
+                <div class="social-icons"> 
+                    <a href="#" class="icon"><i class="fab fa-google"></i></a>
+                    <a href="#" class="icon"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
                 </div>
-                <span>or use your email password</span>
-                <input type="email" placeholder="Email">
-                <input type="password" placeholder="Password">
-                <a href="#">Forget Your Password?</a>
-                <button>Sign In</button>
+                <span>or use your email & password</span><br>
+                <input type="text" name="username" placeholder= "Name"/><br>
+                <input type="password" name="password" placeholder= "Password"/><br>
+                <a href="#">Forget Your Password?</a><br>
+                <button class="form-button">Sign In</button>
             </form>
         </div>
         <div class="toggle-container">
-            <div class="toggle">
-                <div class="toggle-panel toggle-left">
-                    <h1>Welcome Back!</h1>
-                    <p>Enter your personal details to use all of site features</p>
-                    <button class="hidden" id="login">Sign In</button>
-                </div>
+            <div class="toggle>
                 <div class="toggle-panel toggle-right">
-                    <h1>Hello, Friend!</h1>
-                    <p>Register with your personal details to use all of site features</p>
+                    <img src="Assets/images/welcome.jpg" alt="" width=330/>
+                    <h1>Meet your new furry friend</h1>
+                    <p>Register with your personal details to discover pets which are waiting for you!</p>
                     <button class="hidden" id="register">Sign Up</button>
                 </div>
-            </div>
+                <div class="toggle-panel toggle-left">
+                    <img src="Assets/images/signin-dog.jpg" alt="" width=330/>
+                    <h1>Support Sustainable Pet Adoption</h1>
+                    <p>Adopt a pet, Transform a Life</p>
+                    <button class="hidden" id="register">Sign In</button>
+                </div>
+            </div>  
         </div>
     </div>
 
-    <script src="script.js"></script> -->
+
+    <!-- <script src="script.js"></script> --> 
     
 </body>
 </html>
