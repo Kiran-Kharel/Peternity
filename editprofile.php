476e24909734671e4626d066b729e41fa039e318
LasataBjr <lasatabjrktm@gmail.com>
2025-06-03 12:32:16 +0545

    update edit profile add change password page & PHPMailer

--- editprofile.php
+++ editprofile.php
@@ -20,8 +20,7 @@
 	// 	}
     
     if(isset($_POST['submit']))
-	{
-        
+	{        
       	$userid = $_POST['user_id'];	
 		$name = $_POST['name'];
         $address = $_POST['address'];
@@ -94,18 +93,59 @@
 		
 	}		
 ?>
-<div class="container edit-form">
-            <form method="POST" enctype="multipart/form-data">
-                <h1>Edit Profile</h1>     
-                    <input type="hidden" name="user_id" value="<?php echo $userId;?>"/> 
-                    <input type="hidden" name="old_userprofile" value="<?php echo $userprofile;?>"/>     
-                    <input type="text" name="name" placeholder= "Name" value="<?php echo $userName?>"/><br>
-                    <label for="userProfile">User Profile:</label><br>
-                    <img id="pp" src="Assets/uploads/<?= $userprofile;?>" alt="Profile Pic" width=80px height=80px></br></br>
-                    <label for="userProfile">Change Profile:</label><br>
-                    <input type="file" name="userProfile" placeholder= "Profile Picture" accept="image/*"/><br>
-                    <input type="text" name="address" placeholder= "Address" value="<?php echo $userAddress?>"/><br>
-                    <input type="email" name="email" placeholder= "Email" value="<?php echo $userEmail?>"/><br>
-                    <button type="submit" name="submit" class="form-button">Save Changes</button><br>
-            </form>
-</div>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+        <link rel="stylesheet" href="Assets/css/style.css">
+		<link rel="stylesheet" href="Assets/css/editprofile.css">
+        <title>Change Password</title>
+    </head>
+    <body>
+		<div class="edit-profile-wrapper">
+			<div class="slideshow">
+				<img src="Assets/images/welcome.jpg" class="slide active">
+				<img src="Assets/images/05_Rabbit_S_Nemo_2262.jpg.webp" class="slide">
+				<img src="Assets/images/cats-8105667_1280.jpg" class="slide">
+			</div>
+			<div class="edit-form">
+						<form method="POST" enctype="multipart/form-data">
+							<h1>Edit Profile</h1>     
+								<input type="hidden" name="user_id" value="<?php echo $userId;?>"/> 
+								<input type="hidden" name="old_userprofile" value="<?php echo $userprofile;?>"/>     
+								<input type="text" name="name" placeholder= "Name" value="<?php echo $userName?>"/>
+								<label for="userProfile">User Profile:</label>
+								<img id="pp" src="Assets/uploads/<?= $userprofile;?>" alt="Profile Pic" width=200px height=200px>
+								<label for="userProfile">Change Profile:</label>
+								<input type="file" name="userProfile" placeholder= "Profile Picture" accept="image/*"/>
+								<input type="text" name="address" placeholder= "Address" value="<?php echo $userAddress?>"/>
+								<input type="email" name="email" placeholder= "Email" value="<?php echo $userEmail?>"/>
+								<button type="submit" name="submit" class="form-button">Save Changes</button>
+						</form>
+			</div>
+		</div>
+    </body>
+	<script>
+		document.querySelector('input[name="userProfile"]').addEventListener('change', function(e) {
+			const file = e.target.files[0];
+			if (file) {
+				document.getElementById('pp').src = URL.createObjectURL(file); //creates a temporary local URL pointing to the selected image file so it can be previewed in the browser.
+			}
+		});
+
+		const slides = document.querySelectorAll('.slide');
+		let index = 0;
+
+		function showSlide(i) {
+			slides.forEach((slide, idx) => {
+				slide.classList.remove('active');
+				if (idx === i) slide.classList.add('active');
+			});
+		}
+		setInterval(() => {
+			index = (index + 1) % slides.length;
+			showSlide(index);
+		}, 3000);
+	</script>
+</html>
--- userprofile.php
+++ userprofile.php
@@ -54,6 +54,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="Assets/css/style.css">
     <link rel="stylesheet" href="Assets/css/navbar.css">
     <link rel="stylesheet" href="Assets/css/petlist.css">
     <link rel="stylesheet" href="Assets/css/userprofile.css">
@@ -83,7 +84,7 @@
                 <p><strong>Email:</strong>&nbsp;<?php echo $userEmail; ?></p>
                 <p><strong>Address:</strong>&nbsp;<?php echo $userAddress; ?></p>
                 <a href="editprofile.php" class="edit-btn">Edit Profile</a>
-                <a href="#" class="password-btn">Change Password</a>
+                <a href="changePassword.php" class="password-btn">Change Password</a>
             </div>
         </div>
         <section class="card-section">
