777a0b3d7baac00f807eef4cace6925ab02b8888
LasataBjr <lasatabjrktm@gmail.com>
2025-06-08 19:26:17 +0545

    added about section

---
+++ about.php
@@ -0,0 +1,19 @@
+<section style="padding: 4rem;">
+  <div style="display: flex; flex-wrap: wrap; align-items: center; gap: 3rem; max-width: 1200px; margin: auto;">
+    
+    <div style="flex: 1 1 400px; padding-right:10px;">
+      <h2 style="font-size: 3rem; font-weight: bold;">About Our Mission</h2>
+      <p style="font-size: 1.2rem; line-height: 1.6; margin: 1rem 0;">
+        At the Pet Adoption Portal, we are committed to facilitating the adoption of street animals in need of loving homes. 
+        Our goal is to raise awareness about animal welfare issues and encourage responsible pet ownership. 
+        Join us in making a difference in the lives of these deserving animals.
+      </p>
+      <img src="Assets/images/hamster.jpg" alt="Hamster in a wheel" style="max-width: 100%; border-radius: 10px; ">
+    </div>
+
+    <div style="flex: 1 1 400px; padding-left:10px;">
+      <img src="Assets/images/feeding.jpg" alt="Pet Toys" style="max-width: 100%; border-radius: 10px;">
+    </div>
+
+  </div>
+</section>
